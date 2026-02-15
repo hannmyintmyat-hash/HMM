90c5de190e94709c1368ca10ca585374d4449b42
hannmyintmyat-hash <hannmyintmyat@icloud.com>
2026-02-15 08:08:02 +0630

    Initialize update-api-schemas-bb650b299591d141ed3d4eed68232688877fdae3

---
+++ src/util/api.ts
@@ -0,0 +1,19 @@
+import SwaggerParser from "@apidevtools/swagger-parser";
+import type { OpenAPI } from "openapi-types";
+
+const COMMIT = "bb650b299591d141ed3d4eed68232688877fdae3";
+
+let schema: OpenAPI.Document | undefined;
+
+export const getSchema = async () => {
+	if (!schema) {
+		const response = await fetch(
+			`https://gh-code.developers.cloudflare.com/cloudflare/api-schemas/${COMMIT}/openapi.json`,
+		);
+		const obj = await response.json();
+
+		schema = await SwaggerParser.dereference(obj);
+	}
+
+	return schema;
+};

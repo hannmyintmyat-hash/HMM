653c99b78ef2309287b18554cd92ceb47d94b3f1
hannmyintmyat-hash <hannmyintmyat@icloud.com>
2026-02-15 08:08:03 +0630

    Initialize kate/fixes-entraid

---
+++ ec.config.mjs
@@ -0,0 +1,29 @@
+// @ts-check
+import darkTheme from "solarflare-theme/themes/cloudflare-dark-color-theme.json" with { type: "json" };
+import lightTheme from "solarflare-theme/themes/cloudflare-light-color-theme.json" with { type: "json" };
+
+import pluginWorkersPlayground from "./src/plugins/expressive-code/workers-playground.js";
+import pluginOutputFrame from "./src/plugins/expressive-code/output-frame.js";
+import pluginDefaultTitles from "./src/plugins/expressive-code/default-titles.js";
+
+import { pluginCollapsibleSections } from "@expressive-code/plugin-collapsible-sections";
+
+export default {
+	plugins: [
+		pluginWorkersPlayground(),
+		pluginOutputFrame(),
+		pluginDefaultTitles(),
+		pluginCollapsibleSections(),
+	],
+	themes: [darkTheme, lightTheme],
+	styleOverrides: {
+		borderWidth: "1px",
+		borderRadius: "0.25rem",
+		textMarkers: {
+			defaultLuminance: ["32%", "88%"],
+		},
+	},
+	frames: {
+		extractFileNameFromCode: false,
+	},
+};
